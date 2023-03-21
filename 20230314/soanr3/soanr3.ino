d7157a65898c75d020c167441a2d0ee4b537d2b3
rooprete1cham <125527744+rooprete1cham@users.noreply.github.com>
2023-03-21 21:29:30 +0900

    Update soanr3.ino

--- 20230314/soanr3/soanr3.ino
+++ 20230314/soanr3/soanr3.ino
@@ -1,33 +1,69 @@
-#define TRIG1 3 // 초음파 센서 1번 Trig 핀 번호
-#define ECHO1 4 // 초음파 센서 1번 Echo 핀 번호
+#define TRIG1 1 //초음파센서 1번 trig 핀 번호
+#define ECHO1 1 //초음파센서 1번 trig 핀 번호
 
-void setup() 
-{
+#define TRIG2 2 //초음파센서 1번 trig 핀 번호
+#define ECHO2 2 //초음파센서 1번 trig 핀 번호
+
+#define TRIG3 3 //초음파센서 1번 trig 핀 번호
+#define ECHO3 3 //초음파센서 1번 trig 핀 번호
+
+
+
+void setup() {
   // put your setup code here, to run once:
   pinMode(TRIG1, OUTPUT);
   pinMode(ECHO1,  INPUT);
+  pinMode(TRIG2, OUTPUT);
+  pinMode(ECHO2,  INPUT);
+  pinMode(TRIG3, OUTPUT);
+  pinMode(ECHO3,  INPUT);
+  Serial.begin(115200); //통신속도 15200로 정의
 
-  Serial.begin(115200); // 통신속도를 115200으로 정의함
 }
 
-long sonar1(void)  // 초음파 센서 1번 측정함수
+long sonar1(void) //초음파센서  1번 측정 함수
 {
+  // put your main code here, to run repeatedly:
   long duration, distance;
-  digitalWrite(TRIG1, LOW);
+  digitalWrite(TRIG1, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG1, LOW);
   duration = pulseIn(ECHO1, HIGH);
-  distance = ( (float)(340*duration)/1000)/2;
+  distance = ((float)(340 * duration)/1000)/2;
   return distance;
 }
 
-void loop() 
+long sonar2(void) //초음파센서  1번 측정 함수
 {
   // put your main code here, to run repeatedly:
-  //Serial.print("Duration: ");
+  long duration, distance;
+  digitalWrite(TRIG2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG2, LOW);
+  duration = pulseIn(ECHO2, HIGH);
+  distance = ((float)(340 * duration)/1000)/2;
+  return distance;
+}
+long sonar3(void) //초음파센서  1번 측정 함수
+{
+  // put your main code here, to run repeatedly:
+  long duration, distance;
+  digitalWrite(TRIG3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG3, LOW);
+  duration = pulseIn(ECHO3, HIGH);
+  distance = ((float)(340 * duration)/1000)/2;
+  return distance;
+}
+void loop()
+{ 
+  //Serial.print("Duration:");
   //Serial.println(duration);
   Serial.print("Distance1: ");
-  Serial.println( sonar1() );
+  Serial.println(sonar1());
+  Serial.print("Distance2: ");
+  Serial.println(sonar2());
+  Serial.print("Distance3: ");
+  Serial.println(sonar3());
   delay(500);
-
 }
